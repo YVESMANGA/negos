af7d3a833eff52994250df64bf898b0ff79eba6c
YVESMANGA <yvesmanga734@gmail.com>
2026-02-06 08:25:34 +0000

    Modif 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Negos Internationale",
-  description: "Immobilier & Vehicules",
+  description: "Immobiliers & Vehicules",
 };
 
 export default function RootLayout({
