c8bc7e2d8e3caf1fe9cd369e3d47c684793a1c1e
YVESMANGA <yvesmanga734@gmail.com>
2026-02-10 13:35:59 +0000

    update 1

--- src/data/data.ts
+++ src/data/data.ts
@@ -24,12 +24,58 @@ export const immoData = [
     type: "Location",
     location: "Almadies",
     description:
-      "illa  a louer avec toute les meubles. R+2 sur 450 m2 Zone résidentiel Sécurité H24 Dont 5chambres +SDB 3 salons 1 bureau 1 toilette 3 cuisines Americaine 1 cuisine Africaine Piscine Garage pour 4 véhicules Loyer du mois a 6 millions Avec des papiers justificatifs.",
+      "Villa  a louer avec toute les meubles. R+2 sur 450 m2 Zone résidentiel Sécurité H24 Dont 5chambres +SDB 3 salons 1 bureau 1 toilette 3 cuisines Americaine 1 cuisine Africaine Piscine Garage pour 4 véhicules Loyer du mois a 6 millions Avec des papiers justificatifs.",
     area: "450m²",
     rooms: 14,
     media: {
       images: ["/i4.png"],
       videos: ["/videov2.mp4", "/videov21.mp4", "/videov22.mp4"],
+    },
+  },
+
+  {
+    id: 3,
+    slug: "appartement-f4",
+    title: "Appartement f4 - Corniche des mamelles",
+    price: "800.000 FCFA",
+    type: "Location",
+    location: "Corniche des mamelles",
+    description: "Appartement f4 Au 3eme étage Avec ascenceur ",
+    area: "",
+
+    media: {
+      images: ["/ic.jpeg", "/ic2.jpeg", "/ic3.jpeg", "/ic4.jpeg", "/ic5.jpeg"],
+      videos: [],
+    },
+  },
+  {
+    id: 4,
+    slug: "immeuble-studio",
+    title: "Immeuble Studio 55.000 FCFA la nuitee",
+    price: "55.000 FCFA",
+    type: "Location",
+    location: "Corniche des mamelles",
+    description: "Immeuble studio 55.000 FCFA la nuitee",
+    area: "",
+
+    media: {
+      images: ["/i5.jpeg", "/i6.jpeg", "/i7.jpeg"],
+      videos: ["/video3.mp4"],
+    },
+  },
+  {
+    id: 5,
+    slug: "immeuble-studio",
+    title: "Immeuble Studio ",
+    price: "250.000.000 FCFA",
+    type: "Vente",
+    location: "Corniche des mamelles",
+    description: "Immeuble studio",
+    area: "",
+
+    media: {
+      images: ["/i5.jpeg", "/i6.jpeg", "/i7.jpeg"],
+      videos: ["/video3.mp4"],
     },
   },
 ];
